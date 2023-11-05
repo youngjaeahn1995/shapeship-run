63499925856a11b9324bb9d4f1ddfa5f8a8a746b
danii956 <danii956@yahoo.com>
2023-11-04 20:31:36 -0500

    Added rain

--- scenes/obstacles/rain.gd
+++ scenes/obstacles/rain.gd
@@ -1,12 +1,11 @@
 extends Node
 
 @export var instance_scene: PackedScene
-var instance_num = 20
+var instance_num: int = 20
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	spawn_instances()
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -14,10 +13,13 @@ func _process(delta):
 
 func spawn_instances():
 	var screen_size = get_viewport().get_size()
-	for i in range(instance_num):
-		var instance = instance_scene.instantiate()
-		var random_x = randf_range(0, screen_size.x)
-		
-		# instance.position = Vector2(random_x, -instance.get_ball_height())
-		instance.position = Vector2(random_x, 10)
-		add_child(instance)
+	var instance = instance_scene.instantiate()
+	var random_x = randf_range(0, screen_size.x)
+	
+	instance.position = Vector2(random_x, -instance.get_ball_height())
+
+	add_child(instance)
+
+
+func _on_timer_timeout():
+	spawn_instances() # Replace with function body.
