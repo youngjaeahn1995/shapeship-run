68b0fca0104c6062216fe92c19645feb101426ac
Daniel Ahn <danii@daniels-mbp.lan>
2023-11-15 22:05:46 -0600

    Finished randomizing octogons for fading polygon obstacle

--- scenes/obstacles/fading_polygon.gd
+++ scenes/obstacles/fading_polygon.gd
@@ -2,22 +2,30 @@ extends Node
 
 @export var instance_scene: PackedScene
 var instance_num: int = 20
+var screen_size: Vector2i
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	screen_size = get_viewport().get_size()
 	spawn_instances()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func spawn_instances():
-	var screen_size = get_viewport().get_size()
-	
+func spawn_instances():	
 	for i in range(instance_num):
 		var instance = instance_scene.instantiate()
 		add_child(instance)
-		
+		instance.tween_completed.connect(self._tween_completed)
+
 		var random_x = randf_range(0, screen_size.x)
 		var random_y = randf_range(0, screen_size.y)
 		instance.position = Vector2(random_x, random_y)
+
+func _tween_completed(instance):
+	var random_x = randf_range(0, screen_size.x)
+	var random_y = randf_range(0, screen_size.y)
+	
+	instance.position = Vector2(random_x, random_y)
+	instance.restart_tween()
--- scenes/obstacles/octogon.gd
+++ scenes/obstacles/octogon.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal tween_completed(instance)
+
 var duration = 1.5 : set = set_fade_duration
 var tween: Tween
 
@@ -13,11 +15,14 @@ func _process(delta):
 
 func start_tween():
 	tween = create_tween()
-	tween.set_loops()
 	tween.tween_callback(func(): $CollisionPolygon2D.disabled = true)
 	tween.tween_property($Sprite2D, "modulate", Color(1, 1, 1, 1), duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
 	tween.tween_callback(func(): $CollisionPolygon2D.disabled = false)
 	tween.tween_property($Sprite2D, "modulate", Color(1, 1, 1, 0), 0.5).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_callback(func(): tween_completed.emit(self))
 
 func set_fade_duration(duration):
 	duration = duration
+
+func restart_tween():
+	start_tween()
